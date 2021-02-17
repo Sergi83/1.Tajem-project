82862ca779754358de84068fe67dbff6f4aa797f
Sergi83 <serega.stepanyuk@gmail.com>
2021-02-17 11:32:01 +0200

    Home work from JS basic's lecture

---
+++ JS_basics/inClass/JS_basics_work.js
@@ -0,0 +1,10 @@
+// 4. Запитати в юзера його імя, додати до імені – “Батькович” та вивести його на екран
+
+let userName = prompt("What is your name? ");
+alert(`Hi ${userName} Батькович`);
+
+// 5. Undefined, e is not defined. Спробуємо вивести оголошену але не присвоєну змінну.
+
+// let e;
+// alert(e)
+// console.log(e)
